5dc8f9c6f98813220ab44342bff6ce9d00d0d476
Desarrollador1 <garciafloresangel27@gmaile.com>
2023-06-11 00:32:05 -0600

    Agregado el proyecto de HTML, CSS y JavaScript

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+const $btnSignIn= document.querySelector('.sign-in-btn'),
+      $btnSignUp = document.querySelector('.sign-up-btn'),  
+      $signUp = document.querySelector('.sign-up'),
+      $signIn  = document.querySelector('.sign-in');
+
+document.addEventListener('click', e => {
+    if (e.target === $btnSignIn || e.target === $btnSignUp) {
+        $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active')
+    }
+});
+
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
